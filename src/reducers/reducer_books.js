0d30cc2844f9d95c30cd1668ec428d59ce14e3a3
Hein Lin Aung <heinlinaung6891@gmail.com>
2018-04-01 19:40:01 +0630

    Added Book Reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default () => {
+    return [
+        { title : 'JavaScript : The Good Parts'},
+        { title : 'The King\'s Avatar'},
+        { title : 'Milk & Honey'},
+        { title : 'Kafka on the Shore'},
+    ]
+}
