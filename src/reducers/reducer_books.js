d608d60a0f49ca6447b3a65f31b047d6347f284b
Hein Lin Aung <heinlinaung6891@gmail.com>
2018-04-03 10:06:29 +0630

    Showed Book Detail information and added pages info

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default () => {
 	return [
-		{ title : 'JavaScript : The Good Parts'},
-		{ title : 'The King\'s Avatar'},
-		{ title : 'Milk & Honey'},
-		{ title : 'Kafka on the Shore'}
+		{ title : 'JavaScript : The Good Parts', pages : 100},
+		{ title : 'The King\'s Avatar', pages : 90},
+		{ title : 'Milk & Honey', pages : 80},
+		{ title : 'Kafka on the Shore', pages : 70}
 	];
 };
